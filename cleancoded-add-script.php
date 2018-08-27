f2b894f56b8ebed9839e4fc097a67ca30ffc4505
James <james@cleancoded.com>
2018-08-26 20:36:00 -0500

    Initial commit

---
+++ cleancoded-add-script.php
@@ -0,0 +1,166 @@
+<?php
+/**
+* Plugin Name: CLEANCODED Add Script
+* Plugin URI: https://CLEANCODED.com/
+* Version: 1.0
+* Author: CLEANCODED
+* Author URI: https://CLEANCODED.com/
+* Description: Add extra code or scripts to the header and footer of your WordPress website by hooking into wp_head and wp_footer.
+* License: GPL2
+*/
+
+/**
+* Add Headers and Footers Class
+*/
+class CLEANCODEDAddScript {
+	/**
+	* Constructor
+	*/
+	public function __construct() {
+
+		// Plugin Details
+        $this->plugin               = new stdClass;
+        $this->plugin->name         = 'CLEANCODED-Add-Script'; // Plugin Folder
+        $this->plugin->displayName  = 'Add Script'; // Plugin Name
+        $this->plugin->version      = '1.0';
+        $this->plugin->folder       = plugin_dir_path( __FILE__ );
+        $this->plugin->url          = plugin_dir_url( __FILE__ );
+        $this->plugin->db_welcome_dismissed_key = $this->plugin->name . '_welcome_dismissed_key';
+
+   		// Hooks
+		add_action( 'admin_init', array( &$this, 'CLEANCODED_registerSettings' ) );
+        add_action( 'admin_menu', array( &$this, 'CLEANCODED_adminPanelsAndMetaBoxes' ) );
+ 
+        // Frontend Hooks
+        add_action( 'wp_head', array( &$this, 'CLEANCODED_frontendHeader' ) );
+		add_action( 'wp_footer', array( &$this, 'CLEANCODED_frontendFooter' ) );
+
+		// Filters
+		add_filter( 'dashboard_secondary_items', array( &$this, 'dashboardSecondaryItems' ) );
+	}
+
+    /**
+     * Number of Secondary feed items to show
+     */
+	function dashboardSecondaryItems() {
+		return 6;
+	}
+
+    	/**
+	* Register Settings
+	*/
+	function CLEANCODED_registerSettings() {
+		register_setting( $this->plugin->name, 'ccd_add_header', 'trim' );
+		register_setting( $this->plugin->name, 'ccd_add_footer', 'trim' );
+	}
+
+	/**
+    * Register the plugin settings panel
+    */
+    function CLEANCODED_adminPanelsAndMetaBoxes() {
+    	add_submenu_page( 'options-general.php', $this->plugin->displayName, $this->plugin->displayName, 'manage_options', $this->plugin->name, array( &$this, 'adminPanel' ) );
+	}
+
+    /**
+    * Output the Administration Panel
+    * Save POSTed data from the Administration Panel into a WordPress option
+    */
+    function adminPanel() {
+		// only admin user can access this page
+		if ( !current_user_can( 'administrator' ) ) {
+			echo '<p>' . __( 'Sorry, you are not allowed to access this page.', $this->plugin->name ) . '</p>';
+			return;
+		}
+
+    	// Save Settings
+        if ( isset( $_REQUEST['submit'] ) ) {
+        	// Check nonce
+			if ( !isset( $_REQUEST[$this->plugin->name.'_nonce'] ) ) {
+	        	// Missing nonce
+	        	$this->errorMessage = __( 'nonce field is missing. Settings NOT saved.', $this->plugin->name );
+        	} elseif ( !wp_verify_nonce( $_REQUEST[$this->plugin->name.'_nonce'], $this->plugin->name ) ) {
+	        	// Invalid nonce
+	        	$this->errorMessage = __( 'Invalid nonce specified. Settings NOT saved.', $this->plugin->name );
+        	} else {
+	        	// Save	
+				// so do nothing before saving
+	    		update_option( 'ccd_add_header', $_REQUEST['ccd_add_header'] );
+	    		update_option( 'ccd_add_footer', $_REQUEST['ccd_add_footer'] );
+	    		update_option( $this->plugin->db_welcome_dismissed_key, 1 );
+				$this->message = __( 'Settings Saved.', $this->plugin->name );
+			}
+        }
+
+        // Get latest settings
+        $this->settings = array(
+			'ccd_add_header' => esc_html( wp_unslash( get_option( 'ccd_add_header' ) ) ),
+			'ccd_add_footer' => esc_html( wp_unslash( get_option( 'ccd_add_footer' ) ) ),
+        );
+
+    	// Load Settings Form
+        include_once( WP_PLUGIN_DIR . '/' . $this->plugin->name . '/views/settings.php' );
+    }
+
+    /**
+	* Loads plugin textdomain
+	*/
+	function loadLanguageFiles() {
+		load_plugin_textdomain( $this->plugin->name, false, $this->plugin->name . '/languages/' );
+	}
+
+	/**
+	* Outputs script / CSS to the frontend header
+	*/
+	function CLEANCODED_frontendHeader() {
+		$this->output( 'ccd_add_header' );
+	}
+
+	/**
+	* Outputs script / CSS to the frontend footer
+	*/
+	function CLEANCODED_frontendFooter() {
+		$this->output( 'ccd_add_footer' );
+	}
+
+	/**
+	* Outputs the given setting, if conditions are met
+	*
+	* @param string $setting Setting Name
+	* @return output
+	*/
+	function output( $setting ) {
+		// Ignore admin, feed, robots or trackbacks
+		if ( is_admin() || is_feed() || is_robots() || is_trackback() ) {
+			return;
+		}
+
+		// provide the opportunity to Ignore ccd - both headers and footers via filters
+		if ( apply_filters( 'disable_ccd', false ) ) {
+			return;
+		}
+
+		// provide the opportunity to Ignore ccd - footer only via filters
+		if ( 'ccd_add_footer' == $setting && apply_filters( 'disable_ccd_footer', false ) ) {
+			return;
+		}
+
+		// provide the opportunity to Ignore ccd - header only via filters
+		if ( 'ccd_add_header' == $setting && apply_filters( 'disable_ccd_header', false ) ) {
+			return;
+		}
+
+		// Get meta
+		$meta = get_option( $setting );
+		if ( empty( $meta ) ) {
+			return;
+		}
+		if ( trim( $meta ) == '' ) {
+			return;
+		}
+
+		// Output
+		echo wp_unslash( $meta );
+	}
+}
+
+$ccd = new CLEANCODEDAddScript();
---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== Add Headers and Footers ===
+Contributors: CLEANCODED
+Tags: code, content, facebook pixel, google analytics, css, footer, footer code, footer scripts, footers, head, header, header code, header scripts, headers, Add, Add code, Add scripts, js, meta, meta tags, scripts, wpmu
+Requires at least: 3.6
+Tested up to: 4.9.6
+Stable tag: 1.4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easily add extra code and scripts to the header and footer of your WordPress website by hooking into wp_head and wp_footer.
+
+== Description ==
+
+= Add Header and Footer Code =
+
+Add Headers and Footers is a simple plugin that makes it easy to add scripts and code like a Facebook pixel, Google Analytics or custom CSS to your WordPress site header and/or footer, without needing to make edits to your theme files.
+
+This plugin provides a simple dashboard from the WordPress backend dashboard to insert scripts or code to your website.
+
+= Plugin Features =
+
+* Easy to set up in just a few minutes
+* Add code and scripts to your WordPress website
+* Add header code and/or footer code
+* Add <strong>Facebook pixel</strong> code to any theme
+* Add <strong>Google Analytics</strong> code to any theme
+* Add <strong>custom CSS</strong> to any theme
+* Add any code or script, including HTML and Javascript, to any theme
+
+== Installation ==
+
+1. Install Add Headers and Footers by uploading the `Add-headers-and-footers` directory to the `/wp-content/plugins/` directory. 
+2. Activate Add Headers and Footers through the `Plugins` menu in WordPress.
+3. Add code to your header or footer by going to the `Settings > Add Headers and Footers` menu.
+
+== Screenshots ==
+
+1. Settings Screen
+
+== Frequently Asked Questions ==
+
+= Can I use Add Headers and Footers to install Google Analytics? =
+
+Yes, you can Add your Google Analytics code in the `Scripts in Header` field.
+
+= Can I use Add Headers and Footers for Google AdSense? =
+
+Yes, to verify your account or to tag your page for Auto ads, paste the code AdSense gives you, into the Scripts in Header field.
+
+= How to disable Add Headers and Footers on a specific page? =
+
+You can use one of the three available boolean filters: disable_ihaf, disable_ihaf_footer, disable_ihaf_header and return true value in order to disable printing either on the entire page or specifically in the header or footer of a given page.
+
+== Notes ==
+Add Headers and Footers is the easiest way to Add code in your WordPress headers and footers.
+
+CLEANCODED
+
+= 1.0 =
+* Initial version
